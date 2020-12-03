70365319595208705dbbf128ace33848983611e5
yukixia <yxhan97@163.com>
2020-12-03 08:38:26 +0800

    bugfix

---
+++ Week02/go.mod
@@ -0,0 +1,3 @@
+module Week02
+
+go 1.13
--- Week02/service/userservice.go
+++ Week02/service/userservice.go
@@ -2,11 +2,12 @@ package service
 
 import "github.com/yukixia/Go-000/Week02/dao"
 
-type UserService struct {
-	user *dao.UserEntity
-}
+// type UserService struct {
+// 	user *dao.UserEntity
+// }
 
 func GetUser(id int) (*dao.UserEntity, error) {
-	_, err := dao.SelectById(id)
+	user := &dao.UserEntity{}
+	_, err := user.SelectById(id)
 	return nil, err
 }
